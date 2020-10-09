65177fcc628b74900abe40cd947d35397e1d1d17
Tokunbo Akanni <oakanni@gatech.edu>
2020-10-09 15:04:22 -0500

    User input for Current and Job Offer details

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJobDetails.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/CurrentJobDetails.java
@@ -9,13 +9,34 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CurrentJobDetails extends AppCompatActivity {
+    private EditText jobTitle;
+    private EditText jobCompany;
+    private EditText jobCity;
+    private EditText jobState;
+    private EditText jobCostOfLiving;
+    private EditText jobCommute;
+    private EditText jobSalary;
+    private EditText jobBonus;
+    private EditText jobRetirementBenefits;
+    private EditText jobLeaveTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.current_job_details);
 
-        Button currentSave = (Button) findViewById(R.id.currentSaveButtonID);
+        jobTitle = (EditText) findViewById(R.id.currentTitleID);
+        jobCompany = (EditText) findViewById(R.id.currentCompanyID);
+        jobCity = (EditText) findViewById(R.id.currentCityID);
+        jobState = (EditText) findViewById(R.id.currentStateID);
+        jobCostOfLiving = (EditText) findViewById(R.id.currentCostID);
+        jobCommute = (EditText) findViewById(R.id.currentCommuteID);
+        jobSalary = (EditText) findViewById(R.id.currentSalaryID);
+        jobBonus = (EditText) findViewById(R.id.currentBonusID);
+        jobRetirementBenefits = (EditText) findViewById(R.id.currentRetirementID);
+        jobLeaveTime = (EditText) findViewById(R.id.currentLeaveID);
+
+        //Button currentSave = (Button) findViewById(R.id.currentSaveButtonID);
         Button currentCancel = (Button) findViewById(R.id.currentCancelButtonID);
         currentCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,13 +46,51 @@ public class CurrentJobDetails extends AppCompatActivity {
                 CurrentJobDetails.this.finish();
             }
         });
-        currentSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent retToMain = new Intent(CurrentJobDetails.this, MainMenu.class);
-                startActivity(retToMain);
-                CurrentJobDetails.this.finish();
-            }
-        });
     }
+    public void handleClickSaveCurrent (View view){
+        String inputTitle = jobTitle.getText().toString();
+        String inputCompany = jobCompany.getText().toString();
+        String inputCity = jobCity.getText().toString();
+        String inputState = jobState.getText().toString();
+        String inputCostOfLiving = jobCostOfLiving.getText().toString();
+        String inputCommute = jobCommute.getText().toString();
+        String inputSalary = jobSalary.getText().toString();
+        String inputBonus = jobBonus.getText().toString();
+        String inputRetirementBenefits = jobRetirementBenefits.getText().toString();
+        String inputLeaveTime = jobLeaveTime.getText().toString();
+
+        boolean validTitle = inputTitle.length() > 0;
+        boolean validCompany = inputCompany.length() > 0;
+        boolean validCity = inputCity.length() > 0;
+        boolean validState = inputState.length() > 0;
+        boolean validCostOfLiving = inputCostOfLiving.length() > 0;
+        boolean validCommute = inputCommute.length() > 0;
+        boolean validSalary = inputSalary.length() > 0;
+        boolean validBonus = inputBonus.length() > 0;
+        boolean validRetirementBenefits = inputRetirementBenefits.length() > 0;
+        boolean validLeaveTime = inputLeaveTime.length() > 0;
+
+        boolean isCityAllAlphabet = isAlpha(inputCity);
+        boolean isStateAllAlphabet = isAlpha(inputState);
+
+        if (!validTitle){jobTitle.setError("No Title Input");}
+        if (!validCompany){jobCompany.setError("No Company Input");}
+        if (!validCity){jobCity.setError("No City Input");}
+        if (!validState){jobState.setError("No State Input");}
+        if (!validCostOfLiving){jobCostOfLiving.setError("No Cost of Living Index Input");}
+        if (!validCommute){jobCommute.setError("No Commute Input");}
+        if (!validSalary){jobSalary.setError("No Salary Input");}
+        if (!validBonus){jobBonus.setError("No Bonus Input");}
+        if (!validRetirementBenefits){jobRetirementBenefits.setError("No Retirement Benefits Input");}
+        if (!validLeaveTime){jobLeaveTime.setError("No Leave Time Input");}
+        if (!isCityAllAlphabet){jobCity.setError("Invalid City Input");}
+        if (!isStateAllAlphabet){jobState.setError("Invalid State Input");}
+
+    }
+
+    // To check if all characters in string are alphabets
+    public boolean isAlpha(String name) {
+        return name.matches("^[a-zA-Z\\s\\-\']*$");
+    }
+
 }
--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOfferDetails.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOfferDetails.java
@@ -4,17 +4,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class JobOfferDetails extends AppCompatActivity {
+    private EditText jobTitle;
+    private EditText jobCompany;
+    private EditText jobCity;
+    private EditText jobState;
+    private EditText jobCostOfLiving;
+    private EditText jobCommute;
+    private EditText jobSalary;
+    private EditText jobBonus;
+    private EditText jobRetirementBenefits;
+    private EditText jobLeaveTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.job_offer_details);
 
-        Button offerSave = (Button) findViewById(R.id.offerSaveButtonID);
+        jobTitle = (EditText) findViewById(R.id.offerTitleID);
+        jobCompany = (EditText) findViewById(R.id.offerCompanyID);
+        jobCity = (EditText) findViewById(R.id.offerCityID);
+        jobState = (EditText) findViewById(R.id.offerStateID);
+        jobCostOfLiving = (EditText) findViewById(R.id.offerCostID);
+        jobCommute = (EditText) findViewById(R.id.offerCommuteID);
+        jobSalary = (EditText) findViewById(R.id.offerSalaryID);
+        jobBonus = (EditText) findViewById(R.id.offerBonusID);
+        jobRetirementBenefits = (EditText) findViewById(R.id.offerRetirementID);
+        jobLeaveTime = (EditText) findViewById(R.id.offerLeaveID);
+
         Button offerCancel = (Button) findViewById(R.id.offerCancelButtonID);
         offerCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +45,7 @@ public class JobOfferDetails extends AppCompatActivity {
                 JobOfferDetails.this.finish();
             }
         });
+        /*Button offerSave = (Button) findViewById(R.id.offerSaveButtonID);
         offerSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +53,53 @@ public class JobOfferDetails extends AppCompatActivity {
                 startActivity(goToSavedOffer);
                 JobOfferDetails.this.finish();
             }
-        });
+        });*/
     }
+    public void handleClickSaveOffer (View view){
+        String inputTitle = jobTitle.getText().toString();
+        String inputCompany = jobCompany.getText().toString();
+        String inputCity = jobCity.getText().toString();
+        String inputState = jobState.getText().toString();
+        String inputCostOfLiving = jobCostOfLiving.getText().toString();
+        String inputCommute = jobCommute.getText().toString();
+        String inputSalary = jobSalary.getText().toString();
+        String inputBonus = jobBonus.getText().toString();
+        String inputRetirementBenefits = jobRetirementBenefits.getText().toString();
+        String inputLeaveTime = jobLeaveTime.getText().toString();
+
+        boolean validTitle = inputTitle.length() > 0;
+        boolean validCompany = inputCompany.length() > 0;
+        boolean validCity = inputCity.length() > 0;
+        boolean validState = inputState.length() > 0;
+        boolean validCostOfLiving = inputCostOfLiving.length() > 0;
+        boolean validCommute = inputCommute.length() > 0;
+        boolean validSalary = inputSalary.length() > 0;
+        boolean validBonus = inputBonus.length() > 0;
+        boolean validRetirementBenefits = inputRetirementBenefits.length() > 0;
+        boolean validLeaveTime = inputLeaveTime.length() > 0;
+
+        boolean isCityAllAlphabet = isAlpha(inputCity);
+        boolean isStateAllAlphabet = isAlpha(inputState);
+
+        if (!validTitle){jobTitle.setError("No Title Input");}
+        if (!validCompany){jobCompany.setError("No Company Input");}
+        if (!validCity){jobCity.setError("No City Input");}
+        if (!validState){jobState.setError("No State Input");}
+        if (!validCostOfLiving){jobCostOfLiving.setError("No Cost of Living Index Input");}
+        if (!validCommute){jobCommute.setError("No Commute Input");}
+        if (!validSalary){jobSalary.setError("No Salary Input");}
+        if (!validBonus){jobBonus.setError("No Bonus Input");}
+        if (!validRetirementBenefits){jobRetirementBenefits.setError("No Retirement Benefits Input");}
+        if (!validLeaveTime){jobLeaveTime.setError("No Leave Time Input");}
+        if (!isCityAllAlphabet){jobCity.setError("Invalid City Input");}
+        if (!isStateAllAlphabet){jobState.setError("Invalid State Input");}
+
+    }
+
+    // To check if all characters in string are alphabets
+    public boolean isAlpha(String name) {
+        return name.matches("^[a-zA-Z\\s\\-\']*$");
+    }
+
+
 }
