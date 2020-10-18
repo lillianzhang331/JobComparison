2f959b7fa44b339610e26e63af7bd10ff8917a4d
Tokunbo Akanni <oakanni@gatech.edu>
2020-10-18 01:43:58 -0500

    test1 update in test (NullPointerException)

--- GroupProject/JobCompare6300/app/src/test/java/edu/gatech/seclass/jobcompare6300/UnitTests.java
+++ GroupProject/JobCompare6300/app/src/test/java/edu/gatech/seclass/jobcompare6300/UnitTests.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import edu.gatech.seclass.jobcompare6300.model.ComparisonSettingsModel;
 import edu.gatech.seclass.jobcompare6300.model.CurrentJob;
 import edu.gatech.seclass.jobcompare6300.model.JobCompareDbHelper;
 import edu.gatech.seclass.jobcompare6300.model.JobDetails;
@@ -16,23 +17,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class UnitTests extends AppCompatActivity {
 
-    private MyApplication myApplication;
+    //private MyApplication myApplication;
     private JobCompareDbHelper dbHelper;
-    private JobManager job;
+    //private JobManager job;
+    //private CurrentJob currentJob;
+    //private ComparisonSettingsModel comparisonSettingsModel;
 
     //Test job score calculated for current job details from default weights
     @Test
     public void test1() {
-        // INCOMPLETE
-        // INCOMPLETE
-        myApplication = (MyApplication)getApplication();
-        dbHelper = new JobCompareDbHelper(this);
-        job = new JobManager(dbHelper);
-
-        CurrentJob currentJob = job.getCurrentJob();
+        CurrentJob currentJob = new CurrentJob(dbHelper);
         RankedJobs rankedJobs = new RankedJobs();
         //CurrentJobDetails currentJob = new CurrentJobDetails();
-        
+        JobManager job = new JobManager(dbHelper);
         currentJob.setTitle("ML Engineer");
         currentJob.setCompany("Tesla");
         currentJob.setCity("San Francisco");
@@ -44,17 +41,11 @@ public class UnitTests extends AppCompatActivity {
         currentJob.setRetirementBenefits(50);
         currentJob.setLeaveTime(20);
 
-        myApplication.getCommuteWeight();
+        ComparisonSettingsModel comparisonSettingsModel = new ComparisonSettingsModel(dbHelper);
+        comparisonSettingsModel.setComparisonSettings(1,1,1, 1,1);
 
-        ComparisonSettings comparisonSettings = new ComparisonSettings();
-        comparisonSettings.setCommuteWeight(1);
-        comparisonSettings.setSalaryWeight(1);
-        comparisonSettings.setBonusWeight(1);
-        comparisonSettings.setRetirementbenefitsWeight(1);
-        comparisonSettings.setLeaveWeight(1);
-
-        double expectedJobScore = 111111.1; //NOT YET CALCULATED (placeholder)
-        double score = rankedJobs.calcCurrentJobScore(currentJob, comparisonSettings); //NO jobScore method yet
+        double expectedJobScore = 1016137.887;
+        double score = rankedJobs.calcCurrentJobScore(currentJob);
         Assert.assertEquals(expectedJobScore, score, 0.0000001);
     }
 
